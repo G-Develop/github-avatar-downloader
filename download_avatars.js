99c6496fee69711e0d14bea749e4447898cc6b3d
G-Develop <mfaisal.rahman51.996@gmail.com>
2018-09-05 01:29:14 +0000

    print each avatar_url completed

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,29 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-request.get('https://api.github.com/repos/jquery/jquery/contributors')
-  .on('error', function(err) {
-    throw err;
-  })
-  .on('response', function(response) {
-    console.log('Response Status Code: ', response.statusCode);
-  })
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization' : 'GITHUB_TOKEN'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    let parsedJson = JSON.parse(body);
+    let arr = [];
+    parsedJson.forEach(object => {
+      arr.push(object.avatar_url);
+    })
+
+    cb(err, arr);
+    // console.log (parsedJson);
+
+  });
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
