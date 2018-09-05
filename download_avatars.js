187858fe0944f780412b6cf2021d931052c39aa8
G-Develop <mfaisal.rahman51.996@gmail.com>
2018-09-05 00:34:56 +0000

    starting the getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,10 @@
-var reqeust = require('request');
+var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
-
+request.get('https://api.github.com/repos/jquery/jquery/contributors')
+  .on('error', function(err) {
+    throw err;
+  })
+  .on('response', function(response) {
+    console.log('Response Status Code: ', response.statusCode);
+  })
